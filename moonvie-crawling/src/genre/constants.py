45ad35af53051a57cb36fd0e8882d29eeb9ae164
qjrm1430 <qjrm1430@naver.com>
2024-08-16 21:49:25 +0900

    fix: get_movie_list feature 데이터 수집 수정

--- moonvie-crawling/src/genre/constants.py
+++ moonvie-crawling/src/genre/constants.py
@@ -5,3 +5,9 @@ class Parameters(Enum):
     ITEM_PER_PAGE = 100  # 페이지당 출력할 항목 수
     OPEN_START_DT = "2017"
     OPEN_END_DT = "2024"
+
+
+class ApiUrl(Enum):
+    MOVIE_LIST = (
+        "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json"
+    )
--- moonvie-crawling/src/genre/get_movie_list.py
+++ moonvie-crawling/src/genre/get_movie_list.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import ray
 import requests
-from constants import Parameters
+from constants import ApiUrl, Parameters
 from env import settings
 
 ray.init()
@@ -10,9 +10,7 @@ ray.init()
 def request_api(current_page):
     api_key = settings.KOFIC_API_KEY  # 여기에 발급받은 API 키를 입력하세요.
 
-    url = (
-        "http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieList.json"
-    )
+    url = ApiUrl.MOVIE_LIST.value
     params = {
         "key": api_key,
         "curPage": str(current_page),  # 현재 페이지
@@ -45,14 +43,14 @@ def process_get_movie(movie):
         "대표 제작국가": movie.get("repNationNm", "정보 없음"),
         "대표 장르": movie.get("repGenreNm", "정보 없음"),
         # 감독 정보 추출
-        "영화 감독 이름": ", ".join(
+        "영화 감독 이름": ",".join(
             [
                 director.get("peopleNm", "정보 없음")
                 for director in movie.get("directors", [])
             ]
         ),
         # 제작사 정보 추출
-        "제작사 코드": ", ".join(
+        "제작사 코드": ",".join(
             [
                 company.get("companyCd", "정보 없음")
                 for company in movie.get("companys", [])
@@ -62,12 +60,12 @@ def process_get_movie(movie):
     return movie_info
 
 
-def get_movies(movie_list_items):
+def get_page_movies(movie_list_items):
     futures = [process_get_movie.remote(movie) for movie in movie_list_items]
     return ray.get(futures)
 
 
-def get_movie_list() -> None:
+def get_movies() -> None:
     current_page = 1
 
     # 영화 정보를 저장할 리스트 생성
@@ -79,7 +77,9 @@ def get_movie_list() -> None:
         movie_list_items = response_data.get("movieListResult", {}).get("movieList", [])
         try:
             if not movie_list_items:
-                raise Exception(f"현재 페이지: {current_page}에 데이터가 없습니다.")
+                raise Exception(
+                    f"{current_page} 페이지에 데이터가 없습니다.\n 크롤링을 종료합니다."
+                )
             # 데이터 가공
             if response.status_code != 200:
                 raise Exception("API 요청 실패")
@@ -89,30 +89,30 @@ def get_movie_list() -> None:
         else:
             # 데이터 가공
             try:
-                movies.extend(get_movies(movie_list_items))
+                movies.extend(get_page_movies(movie_list_items))
                 # 페이지 번호 증가
                 current_page += 1
 
             except ValueError as e:
-                print(f"JSON 디코딩 오류: {e}")
+                print(f"데이터 가공 실패: {e}")
     return movies
 
 
-def save_movie_list(movies):
+def save_movies2csv(movies):
     # 데이터프레임 생성
-    df = pd.DataFrame(movies)
+    movies_df = pd.DataFrame(movies).sort_values(by="개봉일")
 
     # CSV 파일로 저장
     csv_name = (
-        f"movies{Parameters.OPEN_START_DT.value}_to_{Parameters.OPEN_END_DT.value}.csv"
+        f"movies_{Parameters.OPEN_START_DT.value}_{Parameters.OPEN_END_DT.value}.csv"
     )
-    df.to_csv(csv_name, index=False, encoding="utf-8-sig")
+    movies_df.to_csv(csv_name, index=False, encoding="utf-8-sig")
     print(f"데이터가 '{csv_name}'로 저장되었습니다.")
 
 
 def main():
-    movies = get_movie_list()
-    save_movie_list(movies)
+    movies = get_movies()
+    save_movies2csv(movies)
     ray.shutdown()
 
 
