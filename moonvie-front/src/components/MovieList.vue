f12075d1c9176afa0fb45c2e21fea21fe22ae7e7
JinYunhwa <dbsghk320@gmail.com>
2024-08-16 10:06:46 +0900

    chore: structure 구성

---
+++ moonvie-front/src/components/MovieList.vue
@@ -0,0 +1,44 @@
+<!-- src/components/MovieList.vue -->
+<template>
+    <div class="container mt-5">
+      <h2>영화 목록</h2>
+      <table class="table table-striped">
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th>이름</th>
+            <th>장르</th>
+            <th>개봉일</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="movie in movies" :key="movie.id">
+            <td>{{ movie.id }}</td>
+            <td>{{ movie.name }}</td>
+            <td>{{ movie.genre }}</td>
+            <td>{{ movie.releaseDate }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </template>
+  
+  <script>
+  import { dummyData } from './DummyData';
+  
+  export default {
+    name: 'MovieList',
+    data() {
+      return {
+        movies: dummyData
+      };
+    }
+  };
+  </script>
+  
+  <style scoped>
+  .container {
+    max-width: 800px;
+  }
+  </style>
+  
