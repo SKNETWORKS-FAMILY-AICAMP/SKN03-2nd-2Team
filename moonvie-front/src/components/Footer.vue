691f83e2e1c23aff184756d3cff1193ea19dc301
JinYunhwa <dbsghk320@gmail.com>
2024-08-16 10:10:37 +0900

    chore: main structure 구성

---
+++ moonvie-front/src/components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <footer class="bg-dark text-white text-center py-3">
+    <div class="container">
+      <p>© 2024 Moonvie. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'MoonvieFooter'
+};
+</script>
+
+<style scoped>
+footer {
+  margin-top: auto;
+}
+</style>
