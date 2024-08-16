58eaabb03580013329c8ff37a55241c785321f4a
JinYunhwa <dbsghk320@gmail.com>
2024-08-16 21:05:26 +0900

    -fix: components 구조변경 1. Layout/MainLayout.vue 생성 2. components/MoonvieFooter.vue -> components/Layout/MoonvieFooter.vue 이동 3. components/MoonvieHeader.vue -> components/Layout/MoonvieHeader.vue 이동

--- moonvie-front/src/App.vue
+++ moonvie-front/src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div id="app">
-    <MoonvieHeader />
-    <router-view></router-view>
-    <MoonvieFooter />
+    <mainLayout />
   </div>
 </template>
 
 <script>
-import MoonvieHeader from './components/MoonvieHeader.vue';
-import MoonvieFooter from './components/MoonvieFooter.vue';
+import MainLayout from './components/Layout/MainLayout.vue';
 
 export default {
   name: 'App',
   components: {
-    MoonvieHeader,
-    MoonvieFooter,
+    MainLayout
   }
 };
 </script>
