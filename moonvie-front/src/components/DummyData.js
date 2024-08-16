0d034db6009caa6ee9b18bb41ff3bcc6177f003d
JinYunhwa <dbsghk320@gmail.com>
2024-08-16 10:07:03 +0900

    Revert "chore: structure 구성"
    This reverts commit f12075d1c9176afa0fb45c2e21fea21fe22ae7e7.

--- moonvie-front/src/components/DummyData.js
+++
@@ -1,7 +0,0 @@
-// src/components/DummyData.js
-export const dummyData = [
-    { id: 1, name: '영화 1', genre: '액션', releaseDate: '2023-01-01' },
-    { id: 2, name: '영화 2', genre: '코미디', releaseDate: '2023-02-01' },
-    { id: 3, name: '영화 3', genre: '드라마', releaseDate: '2023-03-01' },
-  ];
-  
