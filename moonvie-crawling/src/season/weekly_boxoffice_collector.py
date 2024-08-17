9226eb887d1ec339b5d6460b18fe7d79d72d7392
Heesun <goddns0272@gmail.com>
2024-08-17 12:06:44 +0900

    feat: add weekly data preprocessing file

--- moonvie-crawling/src/season/weekly_boxoffice_collector.py
+++ moonvie-crawling/src/season/weekly_boxoffice_collector.py
@@ -1,18 +1,25 @@
 import requests
 from datetime import datetime
-from env.api_key import api_key
+from env.api_key import API_KEY
 import csv
 import os
 
 
 # API 호출 함수
-def fetch_movie_data(api_key: str, target_date: str, item_per_page="100", week_gb="0"):
+def fetch_movie_data(
+    api_key: str,
+    target_date: str,
+    region: set,
+    item_per_page="10",
+    week_gb="0",
+):
     url = "http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchWeeklyBoxOfficeList.json"
     params = {
         "key": api_key,
         "targetDt": target_date,
         "itemPerPage": item_per_page,
         "weekGb": week_gb,
+        "wideAreaCd": region[0],
     }
 
     response = requests.get(url, params=params)
@@ -30,7 +37,7 @@ def get_week_start_date(year: str, week: str) -> str:
 
 
 # CSV로 저장하는 함수
-def save_csv(data, csv_file) -> None:
+def save_csv(data, csv_file: csv, region: str) -> None:
     csv_columns = [
         "박스오피스종류",
         "상영기간",
@@ -53,6 +60,7 @@ def save_csv(data, csv_file) -> None:
         "누적 관객수",
         "해당일자 상영 스크린수",
         "해당일자 상영 횟수",
+        "지역",
     ]
 
     file_exists = os.path.isfile(csv_file)
@@ -90,55 +98,64 @@ def save_csv(data, csv_file) -> None:
                         "누적 관객수": movie["audiAcc"],
                         "해당일자 상영 스크린수": movie["scrnCnt"],
                         "해당일자 상영 횟수": movie["showCnt"],
+                        "지역": region,
                     }
                 )
-        print(f"Data successfully saved to {csv_file}")
+        print(f"{yearWeekTime}: Data successfully saved to {csv_file}")
     except IOError as e:
         print(f"Error saving to file: {e}")
 
 
 def main():
     # API 키를 입력하세요
-    api_key = api_key
+    api_key = API_KEY
 
     # 날짜 설정
     start_year = 2017
     end_year = 2024
-    week_gb_options = ["0", "1", "2"]  # 주간(0), 주말(1), 주중(2)
+    regions = [
+        (None, "전국"),
+        ("0105001", "서울시"),
+        ("0105002", "경기도"),
+        ("0105003", "강원도"),
+        ("0105004", "충청북도"),
+        ("0105005", "충청남도"),
+        ("0105006", "경상북도"),
+        ("0105007", "경상남도"),
+        ("0105008", "전라북도"),
+        ("0105009", "전라남도"),
+        ("0105010", "제주도"),
+        ("0105011", "부산시"),
+        ("0105012", "대구시"),
+        ("0105013", "대전시"),
+        ("0105014", "울산시"),
+        ("0105015", "인천시"),
+        ("0105016", "광주시"),
+        ("0105017", "세종시"),
+    ]
 
-    # 추가적인 필터링 옵션 설정
-    item_per_page = "100"  # 페이지 당 결과 수
-    week_gb = "0"
-    """	주간/주말/주중을 선택 입력합니다
-        “0” : 주간 (월~일)
-        “1” : 주말 (금~일) (default)
-        “2” : 주중 (월~목)
-    """
-    # multi_movie_yn = ""  # 다양성 영화 여부 ("Y" or "N", default: 전체)
-    # rep_nation_cd = ""  # 영화 국가 구분 ("K" for 한국영화, "F" for 외국영화, default: 전체)
-    # wide_area_cd = ""  # 상영 지역 코드 (default: 전체)
+    for region in regions:
+        for year in range(start_year, end_year + 1):
+            for week in range(1, 53):  # 1년은 52주차까지 존재
+                target_date = get_week_start_date(year, week)
 
-    for year in range(start_year, end_year + 1):
-        for week in range(1, 53):  # 1년은 52주차까지 존재
-            target_date = get_week_start_date(year, week)
+                try:
+                    # 데이터 호출
+                    movie_data = fetch_movie_data(api_key, target_date, region)
 
-            for week_gb in week_gb_options:
-                # 데이터 호출
-                movie_data = fetch_movie_data(
-                    api_key, target_date, item_per_page, week_gb
-                )
+                    # 현재 경로에 CSV 파일을 생성
+                    current_directory = os.getcwd()
+                    csv_file = os.path.join(
+                        current_directory, f"{region[1]}_WeeklyBoxOfficeData.csv"
+                    )
 
-                # 현재 경로에 CSV 파일을 생성
-                current_directory = os.getcwd() + "/weekly/"
-                csv_file = os.path.join(
-                    current_directory, f"{week_gb}_WeeklyBoxOfficeData.csv"
-                )
-
-                # CSV 파일로 저장
-                if movie_data:
-                    save_csv(movie_data, csv_file)
-                else:
-                    print(f"No data for year {year}, week {week}, weekGb {week_gb}")
+                    # CSV 파일로 저장
+                    if movie_data:
+                        save_csv(movie_data, csv_file, region[1])
+                    else:
+                        print(f"No data for year {year}, week {week}")
+                except KeyError:
+                    print("#" * 20, "Next Region", "#" * 20)
 
 
 if __name__ == "__main__":
