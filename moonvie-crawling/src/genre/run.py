5d783f5cf606edfb1aa758ae159324bf39adda52
jays <js563@daum.net>
2024-08-19 09:29:14 +0900

    feat: add get_company,get_director,get_genre,run.py

--- moonvie-crawling/src/genre/run.py
+++ moonvie-crawling/src/genre/run.py
@@ -1,10 +1,12 @@
 import cleansing_movies
 import get_movies_raw_data
+import get_genre
 
 
 def main():
     get_movies_raw_data.main()
     cleansing_movies.main()
+    get_genre.main()
 
 
 if __name__ == "__main__":
