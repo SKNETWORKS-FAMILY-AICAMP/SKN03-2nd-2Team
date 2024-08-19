a56425577f900b46effb123049ddf4a6499b58c9
Heesun <goddns0272@gmail.com>
2024-08-19 16:04:22 +0900

    fix: delete fron before refactoring

--- moonvie-frontend/src/api/covid.js
+++
@@ -1,8 +0,0 @@
-import { http } from '@/api/axios'
-
-export async function getMoneyCovid() {
-  return await http.get('/covid/money')
-}
-export async function getPeopleCovid() {
-  return await http.get('/covid/people')
-}
